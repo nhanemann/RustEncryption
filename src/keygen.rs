6eca3e2cdf1d3d0070a667c7acce49b5fc2372a1
N <N@NICK-PC>
2019-08-15 08:59:20 -0700

    Updated comments and added tests

--- src/keygen.rs
+++ src/keygen.rs
@@ -1,3 +1,11 @@
+// Copyright © 2019 Nicholas Hanemann
+// [This program is licensed under the "MIT License"]
+// Please see the file LICENSE in the source
+// distribution of this software for license terms.
+
+//This file contains the logic and some necessary math for generating keys.
+//It also contains the relevant tests.
+
 use crate::millerrabin as mrp;
 use num::bigint::BigInt;
 use rand::Rng;
@@ -31,4 +39,35 @@ pub fn keys() -> (u64, u64, BigInt) {
     let l = less_than(p);
     let pm = mrp::modular_exponentiation(&2, &l, &p);
     (p, l, pm)
+}
+
+//--------TESTS BELOW HERE------------------------------
+#[test]
+fn test_57_bit() {
+	for _x in 0..10 {
+		let t_val = r57bit();
+		assert_eq!(true, t_val > 72057594037927936u64);
+		assert_eq!(true, t_val < 144115188075855872u64);
+	}
+}
+
+#[test]
+fn test_big_prime() {
+	for _x in 0..10 {
+		assert_eq!(true, mrp::is_prime(&big_prime()));
+	}
+}
+
+#[test]
+fn test_less_than() {
+	assert_eq!(true, less_than(1000) < 1000);
+	assert_eq!(true, less_than(10) < 10);
+	assert_eq!(true, less_than(144115188075855872u64) < 144115188075855872u64);
+}
+
+#[test]
+fn test_keygen() {
+	let (p, lt, pm) = keys();
+	assert_eq!(true, lt < p);
+	assert_eq!(pm, mrp::modular_exponentiation(&2, &lt, &p));
 }
